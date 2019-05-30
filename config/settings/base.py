f795b101c48c37d8228fecbea7830f829a2f60db
Jochen Wersdoerfer <jochen@wersdoerfer.de>
2019-05-30 19:13:15 +0200

    fix timezone

--- config/settings/base.py
+++ config/settings/base.py
@@ -22,7 +22,7 @@ DEBUG = env.bool('DJANGO_DEBUG', False)
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # though not all of them may be available with every OS.
 # In Windows, this must be set to your system time zone.
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 # https://docs.djangoproject.com/en/dev/ref/settings/#language-code
 # LANGUAGE_CODE = 'en-us'
 LANGUAGE_CODE = 'de-DE'
