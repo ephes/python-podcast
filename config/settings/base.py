cfadb9b0f98e393bafd7a26ecda22a2d6d44d314
Jochen Wersdörfer <jochen@wersdoerfer.de>
2020-07-17 07:22:34 +0200

    readded auto schema to drf

--- config/settings/base.py
+++ config/settings/base.py
@@ -271,7 +271,8 @@ REST_FRAMEWORK = {
  'DEFAULT_AUTHENTICATION_CLASSES': (
      'rest_framework.authentication.SessionAuthentication',
      'rest_framework.authentication.TokenAuthentication',
- )
+ ),
+ 'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
 
 # django imagekit
