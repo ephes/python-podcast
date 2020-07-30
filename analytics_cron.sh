198b7f17977a1c89681996c77a0c832346e0a932
Jochen Wersdörfer <jochen-pythonpodcast@wersdoerfer.de>
2020-07-30 20:20:02 +0200

    use poetry

--- analytics_cron.sh
+++ analytics_cron.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cd $HOME/python-podcast
+poetry shell
 (env $(cat .analytics_env | xargs) /home/python-podcast/.virtualenvs/pp/bin/python manage.py access_log_import 2>&1) > access_log_import.log
