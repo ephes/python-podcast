640757ea9114a440afa0e0a31f9cefab840142ad
Jochen Wersdörfer <jochen@wersdoerfer.de>
2022-11-19 08:00:54 +0100

    #6 fixed backup script

--- scripts/prod_backup.py
+++ scripts/prod_backup.py
@@ -1,4 +1,3 @@
-import os
 from subprocess import check_output
 
 docker_id_cmd = 'docker ps | grep python_podcast_production_postgres | cut -d " " -f 1'
@@ -8,21 +7,22 @@ postgres_id = (check_output(docker_id_cmd, shell=True)
 print(postgres_id)
 
 backup_cmd = 'docker-compose -f production.yml run postgres backup | cut -d " " -f 5'
-backup_name = (check_output(backup_cmd, shell=True)
+backup_lines = (check_output(backup_cmd, shell=True)
                .decode('utf-8')
-               .replace("\n", ""))
-# there are weird escape sequences in backup_name
-backup_name = "".join([c for c in backup_name if c.isalnum() or c in set(['.', '_'])])
+               .split("\n"))
+backup_name = None
+for line in backup_lines:
+    if "backup" in line:
+        # there are weird escape sequences in backup_name
+        backup_name = "".join([c for c in line if c.isalnum() or c in set(['.', '_'])])
+assert backup_name is not None
 print(backup_name)
 
-if not os.path.exists("backups"):
-    os.mkdir("backups")
-
-copy_cmd = "docker cp {}:/backups/{} backups".format(
+copy_cmd = 'docker cp {}:/backups/{} backups'.format(
     postgres_id, backup_name)
 print(copy_cmd)
 result = check_output(copy_cmd, shell=True)
 print(result)
 
-ssh_cmd = f"scp python-podcast:site/backups/{backup_name} backups"
+ssh_cmd = 'scp python-podcast:site/backups/{} backups'.format(backup_name)
 print(ssh_cmd)
