c70615d5ac93eaedfb147ed05a7bf82c94bc749c
Jochen Wersdörfer <jochen@wersdoerfer.de>
2022-11-19 07:01:46 +0100

    #6 added requirements / commands.py

--- commands.py
+++ commands.py
@@ -1,119 +1,193 @@
-import typer
-import environ
-import webbrowser
+import os
+import platform
 import subprocess
-
+import sys
+import webbrowser
 from pathlib import Path
-from functools import wraps
 
 
-env = environ.Env()
-INSIDE_DOCKER = env("INSIDE_DOCKER", default=False)
-DOCKER_COMMAND_PREFIX = "docker-compose -f docker-compose.yml run --rm django /bin/bash -c".split()
-
-COMMANDS = {
-    "unittest_test": "python manage.py test --settings config.settings.test",
-    "pytest_test": "pytest",
-    "flake8": "flake8 .",
-    "black": "black .",
-    "coverage": (
-        "coverage run --source='.' manage.py test --failfast "
-        "--settings=config.settings.test && coverage report && "
-        " coverage html"
-    ),
-    "docs": "cd docs && make html",
-}
+def get_project_root():
+    return Path(__file__).parent.resolve()
 
 
-def typer_cli(f):
-    # @app.command() does not work, dunno why
-    @wraps(f)
-    def wrapper():
-        return typer.run(f)
+def update_via_pip_tools(upgrade: bool):
+    base_command = [sys.executable, "-m", "piptools", "compile"]
+    if upgrade:
+        base_command.append("--upgrade")
+    base_command.extend(
+        [
+            "--allow-unsafe",
+            # "--generate-hashes",  # FIXME does not work with repo urls (django-cast)
+            "requirements/production.in",
+        ]
+    )
 
-    return wrapper
-
-
-def print_styled_command(command):
-    typer.echo(typer.style("Running command line:", fg=typer.colors.WHITE, bg=typer.colors.GREEN, bold=True,))
-    typer.echo(typer.style(" ".join(command), bold=True) + "\n")
-
-
-def run_command(command, debug=False):
-    if not INSIDE_DOCKER:
-        command = [*DOCKER_COMMAND_PREFIX, command]
-    else:
-        command = command.split()
-    if debug:
-        print_styled_command(command)
-    subprocess.run(command)
-
-
-@typer_cli
-def test(test_path: str = typer.Argument(None)):
-    command = COMMANDS["unittest_test"]
-    if test_path is not None:
-        command = f"{command} {test_path}"
-    run_command(command, debug=True)
+    subprocess.call(  # develop + production
+        [
+            *base_command,
+            "requirements/develop.in",
+            "--output-file",
+            "requirements/develop.txt",
+        ]
+    )
+    subprocess.call(  # production only
+        [
+            *base_command,
+            "--output-file",
+            "requirements/production.txt",
+        ]
+    )
+    subprocess.call([sys.executable, "-m", "piptools", "sync", "requirements/develop.txt"])
 
 
-@typer_cli
-def pytest(test_path: str = typer.Argument(None)):
-    command = COMMANDS["pytest_test"]
-    if test_path is not None:
-        command = f"{command} {test_path}"
-    run_command(command, debug=True)
+def bootstrap():
+    """
+    Called when first non-standard lib import fails.
+
+    We need at least pip-tools, typer and rich to use this script.
+    """
+
+    def get_base_prefix_compat():
+        """Get base/real prefix, or sys.prefix if there is none."""
+        return getattr(sys, "base_prefix", None) or getattr(sys, "real_prefix", None) or sys.prefix
+
+    def in_virtualenv():
+        return get_base_prefix_compat() != sys.prefix
+
+    def pip_install(package):
+        subprocess.run([sys.executable, "-m", "pip", "install", package], check=True)
+
+    if not in_virtualenv():
+        print("Please create a virtual environment first and activate it!")
+        sys.exit(1)
+
+    print("Empty virtualenv, installing development requirements..")
+    pip_install("pip-tools")
+    update_via_pip_tools(upgrade=True)
 
 
-@typer_cli
-def flake8():
-    command = COMMANDS["flake8"]
-    run_command(command, debug=True)
+try:
+    import typer
+except ImportError:
+    bootstrap()
+    import typer
+
+from rich import print  # noqa
+
+cli = typer.Typer()
 
 
-@typer_cli
-def black():
-    command = COMMANDS["black"]
-    run_command(command, debug=True)
+def get_pythonpath():
+    """Add project root and model directory to string"""
+    project_root = str(get_project_root())
+    model_root = str(Path(__file__).parent / "model")
+    return f"{project_root}:{model_root}"
 
 
-@typer_cli
+def env_with_pythonpath():
+    """Get en environment dict with includes PYTHONPATH"""
+    env = os.environ.copy()
+    env["PYTHONPATH"] = get_pythonpath()
+    return env
+
+
+@cli.command()
+def mypy():
+    """Run Mypy (configured in pyproject.toml)"""
+    subprocess.call(["mypy", "."])
+
+
+@cli.command()
+def test():
+    subprocess.call(["python", "-m", "pytest"], env=env_with_pythonpath())
+
+
+@cli.command()
 def coverage():
-    command = COMMANDS["coverage"]
-    run_command(command, debug=True)
-    if not INSIDE_DOCKER:
-        file_url = "file://" + str(Path("coverage/index.html").resolve())
-        webbrowser.open_new_tab(file_url)
+    """
+    Run and show coverage.
+    """
+    subprocess.call(["coverage", "run", "-m", "pytest"], env=env_with_pythonpath())
+    subprocess.call(["coverage", "html"])
+    if platform.system() == "Darwin":
+        subprocess.call(["open", "htmlcov/index.html"])
+    elif platform.system() == "Linux" and "Microsoft" in platform.release():  # on WSL
+        subprocess.call(["explorer.exe", r"htmlcov\index.html"])
 
 
-@typer_cli
+@cli.command()
+def jupyterlab():
+    """
+    Start a jupyterlab server.
+    """
+    project_root = get_project_root()
+    notebook_dir = project_root / "notebooks"
+    notebook_dir.mkdir(exist_ok=True)
+    env = env_with_pythonpath() | {"DJANGO_ALLOW_ASYNC_UNSAFE": "true"}
+    subprocess.call([sys.executable, "manage.py", "shell_plus", "--notebook"], env=env)
+
+
+@cli.command()
+def update(upgrade: bool = typer.Option(True, "--upgrade/--no-upgrade")):
+    """
+    Update the development environment by calling:
+    - pip-compile production.in develop.in -> develop.txt
+    - pip-compile production.in -> production.txt
+    - pip-sync develop.txt
+    """
+    update_via_pip_tools(upgrade)
+
+
+@cli.command()
+def clean_build():
+    commands = [
+        ["rm", "-fr", "build/"],
+        ["rm", "-fr", "dist/"],
+        ["rm", "-fr", "*.egg-info"],
+        ["rm", "-fr", "__pycache__"],
+    ]
+    for command in commands:
+        subprocess.call(*command)
+
+
+@cli.command()
+def clean_pyc():
+    commands = [
+        ["find", ".", "-name", "*.pyc", "-exec", "rm -f {} +"],
+        ["find", ".", "-name", "*.pyo", "-exec", "rm -f {} +"],
+        ["find", ".", "-name", "*~", "-exec", "rm -f {} +"],
+    ]
+    for command in commands:
+        subprocess.call(*command)
+
+
+@cli.command()
+def clean():
+    clean_build()
+    clean_pyc()
+
+
+@cli.command()
 def docs():
-    command = COMMANDS["docs"]
-    run_command(command, debug=True)
-    if not INSIDE_DOCKER:
-        file_url = "file://" + str(Path("docs/_build/html/index.html").resolve())
-        webbrowser.open_new_tab(file_url)
+    autogenerated = [
+        "cast.api.rst",
+        "cast.migrations.rst",
+        "cast.rst",
+        "cast.templatetags.rst",
+        "modules.rst",
+    ]
+    for rst_name in autogenerated:
+        (Path("docs") / rst_name).unlink(missing_ok=True)
+    commands = [
+        ["sphinx-apidoc", "-o", "docs/", "cast"],
+        ["make", "-C", "docs", "clean"],
+        ["make", "-C", "docs", "html"],
+    ]
+    for command in commands:
+        subprocess.call(*command)
+    file_url = "file://" + str(Path("docs/_build/html/index.html").resolve())
+    webbrowser.open_new_tab(file_url)
 
 
-@typer_cli
-def command(name: str, debug: bool = False):
-    command = COMMANDS.get(name, 'echo "no such command"')
-    run_command(command, debug=debug)
-
-
-@typer_cli
-def shell():
-    if INSIDE_DOCKER:
-        return
-    command = DOCKER_COMMAND_PREFIX[:-1]
-    print_styled_command(command)
-    subprocess.run(DOCKER_COMMAND_PREFIX[:-1])
-
-
-@typer_cli
-def attach(container_id: str):
-    if INSIDE_DOCKER:
-        return
-    command = f"docker exec -it {container_id} /bin/bash".split()
-    print_styled_command(command)
-    subprocess.run(command)
+if __name__ == "__main__":
+    cli()
