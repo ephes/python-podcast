411ef6c5205f5e2fb0e9102ed34dd3a15fa819dd
Jochen Wersdörfer <jochen@wersdoerfer.de>
2023-03-26 16:25:36 +0200

    added htmx middleware

--- config/settings/base.py
+++ config/settings/base.py
@@ -81,6 +81,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "wagtail.contrib.redirects.middleware.RedirectMiddleware",
+    "django_htmx.middleware.HtmxMiddleware",
 ]
 
 # MIGRATIONS
